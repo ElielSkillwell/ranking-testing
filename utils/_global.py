b5617ac2fc0dd723ba7a171b6c50d2367e6d5a60
Eliel Taskinen <132335968+ElielSkillwell@users.noreply.github.com>
2024-04-11 16:17:58 +0300

    Changed _global variables

--- utils/_global.py
+++ utils/_global.py
@@ -3,9 +3,9 @@ MAX_CHANGES = {"MAX_CHANGE_IN_ELO_UNDER_700": (700, 48),
                 "MAX_CHANGE_IN_ELO_UNDER_1500": (1500, 20)
 }
 MAX_CHANGES_DEFAULT = 8
-MIN_ELO = 100
+MIN_ELO = 200
 ELO_MAGIC_C = 400
 
 
 player_str_mean = 0.5
-player_str_sigma = 0.4
+player_str_sigma = 0.2
